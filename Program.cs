735184eb1abd654520a45d998aa30c47c7a4b1cc
Yohitapolass <warud.seso@mail.kmutt.ac.th>
2023-03-01 01:03:39 +0700

    Add static Main And Int

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PasswordValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 6-digit password: ");
+            int password = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Agency code (CIA, FBI, or NSA): ");
+            string agencyCode = Console.ReadLine().ToUpper();
+        }
+    }
+}
