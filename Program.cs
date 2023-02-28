d6e33427ab62e6015ecade6531366c9d74357447
Yohitapolass <warud.seso@mail.kmutt.ac.th>
2023-03-01 01:33:26 +0700

    Add CIA FBI NSA And If else CIA

--- Program.cs
+++ Program.cs
@@ -45,5 +45,74 @@ namespace PasswordValidation
                 Console.WriteLine("Password is invalid");
             }
         }
+
+        static bool CheckCIA(int password)
+        {
+            int unitDigit = password % 10;
+            int tensDigit = (password / 10) % 10;
+            int thousandsDigit = (password / 1000) % 10;
+
+            if (unitDigit % 3 != 0)
+            {
+                return false;
+            }
+            if (tensDigit == 1 || tensDigit == 3 || tensDigit == 5)
+            {
+                return false;
+            }
+            if (thousandsDigit <= 6 && thousandsDigit == 8)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+         static bool CheckFBI(int password)
+        {
+            int hundredThousandsDigit = (password / 100000);
+            int thousandsDigits = (password / 10000) % 10;
+            int hundredsDigit = (password / 100) % 10;
+
+            if (hundredsDigit % 3 != 0)
+            {
+                return false;
+            }
+            if (hundredsDigit % 3 != 0)
+            {
+                return false;
+            }
+            if (hundredsDigit % 3 != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool CheckNSA(int password)
+        {
+            int unitDigits = password % 10;
+            int hundredsDigits = (password / 100) % 10;
+            int Digit = 7;
+
+            if (unitDigits % 3 != 0)
+            {
+                return false;
+            }
+            if (unitDigits % 3 != 0)
+            {
+                return false;
+            }
+            if (unitDigits % 3 != 0)
+            {
+                return false;
+            }
+
+            return true;
+        } 
+
+
+
     }
 }
